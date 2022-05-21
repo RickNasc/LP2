62850298f17b42ecd22067d193f29e023b731a89
Ricardo Goncalves <ricardo.nascigoncalves@gmail.com>
2022-05-20 22:33:58 -0300

    Add files via upload

---
+++ Prova/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace P1LP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstbxRespalunos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(98, 139);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(139, 114);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lstbxRespalunos
+            // 
+            this.lstbxRespalunos.FormattingEnabled = true;
+            this.lstbxRespalunos.Location = new System.Drawing.Point(335, 12);
+            this.lstbxRespalunos.Name = "lstbxRespalunos";
+            this.lstbxRespalunos.Size = new System.Drawing.Size(249, 368);
+            this.lstbxRespalunos.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 409);
+            this.Controls.Add(this.lstbxRespalunos);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstbxRespalunos;
+    }
+}
+
---
+++ Prova/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P1LP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            string[,] Alunos = new string[10,3];
+            string[] Gabarito = { "E", "B", "A", "A", "C", "B", "D", "E", "B", "A" };
+            string aux;
+
+            int x = 0;
+            int i = 0;
+
+            for (x = 0; x < 10; x++)
+            {
+                for (i = 0; i < 3; i++)
+                {
+                    aux = Interaction.InputBox("Digite a resposta da questão " + (x + 1), "Entrada de Dados");
+                    if (aux == "A" || aux == "B" || aux == "C" || aux =="D" || aux == "E")
+                    {
+
+                        Alunos[x, i] = aux;
+                    }
+                    else{
+                        MessageBox.Show("Valor inválido");
+                        i--;
+                    }
+
+               }
+            }
+            
+            lstbxRespalunos.Items.Add("O Aluno: " + Alunos + " tem ");
+
+            }
+    }
+}
+               
